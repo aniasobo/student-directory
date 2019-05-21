a870614b907b226e689554a42ad327c2cf7ef67c
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-05-21 20:54:11 +0100

    Refactoring

--- directory.rb
+++ directory.rb
@@ -1,41 +1,41 @@
-# basic version, for additional methods see test_directory.rb
-def interactive_menu
-  students = []
-  loop do
-# 1. print options and ask for input
-    puts "1. Input students"
-    puts "2. Show students"
-    puts "9. Exit"
-# 2. save input to variable
-    selection = gets.chomp
-# 3. do the correct action
-    case selection
+# additional methods in test_directory.rb
+@students = []
+
+def print_menu
+  puts "1. Input students"
+  puts "2. Show students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit
     else
       puts "I don't know what that means, try again."
-    end
-  end 
+  end
 end
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
 
 def input_students
   puts "Enter student record:"
   puts "(hit return twice to finish)"
-  students = []
   name = gets.chomp
   while !name.empty? do 
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students."
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students."
     name = gets.chomp
   end
-  students
 end
 
 def print_header
@@ -43,19 +43,21 @@ def print_header
   puts "--------------------------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index| 
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def print_students_list
+  @students.each_with_index do |student, index| 
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students."
+def print_footer
+  puts "Overall, we have #{@students.count} great students."
 end
 
 
 interactive_menu
-# students = input_students
-# print_header
-# print(students)
-# print_footer(students)
